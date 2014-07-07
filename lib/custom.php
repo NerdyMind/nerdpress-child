993b4c93ae591c10a4c56a43ac1b72b5f6d76ba7
Dan Bonomo <d.z.bonomo@gmail.com>
2014-07-07 17:04:02 -0600

    Removes the removal of the link manager
    ┬─┬ノ( º _ ºノ)

--- lib/custom.php
+++ lib/custom.php
@@ -2,9 +2,3 @@
 /**
  * Custom Child functions
  */
-
-/* Gets rid of the link manager on the dashboard. Who uses this anymore?!? */
-function remove_menus(){
-  remove_menu_page( 'link-manager.php' );        //Link Menu
-}
-add_action( 'admin_menu', 'remove_menus' );
